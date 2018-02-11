54e9e5a40a5dd16188dbf9a3957c1c6f38680559
ravikiradoo <kiradooravi@gmail.com>
2018-02-11 14:36:21 +0530

    Adding Navbar

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import web
 urls=[
     '/(.*)','index'
 ]
-render = web.template.render("resources/")
+render = web.template.render("views/Templates",base="MainLayout")
 app = web.application(urls,globals())
 class index:
     def GET(self,name):
-        return render.main(name)
+        return render.Home(name)
 
 if __name__=="__main__":
     app.run()
