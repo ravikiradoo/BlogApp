c9bc72882c1cabb908a6f32583ec1eb72f5179fb
ravikiradoo <kiradooravi@gmail.com>
2018-02-12 16:35:23 +0530

    Add user pymongo

--- models/Register.py
+++ models/Register.py
@@ -2,5 +2,11 @@ import pymongo
 from pymongo import MongoClient
 
 class RegisterModel:
+
+    def __init__(self):
+        self.client=MongoClient()
+        self.db=self.client.Blogapp
+        self.Users=self.db.users
     def insert_user(self,data):
-        print("Data",data)
+        id=self.Users.insert({"Email":data.email,"Password":data.password})
+        print(id)
